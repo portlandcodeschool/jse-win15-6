34fada1ce2cd33e9bccf975af98358f6c5a3999f
Danniel Rolfe <info@dannielrolfe.com>
2015-02-12 17:40:38 -0800

    Worked on problem number 2

---
+++ dannielRolfe/solution-2.js
@@ -0,0 +1,69 @@
+//IFEE 
+
+var Animal = (function() {
+	//Constructor 
+	function Animal(name){
+		this.name = name; 
+
+		Animal.prototype.move = function() {
+			return "Walk";
+			} 
+
+			Animal.prototype.look = function() {
+				return "Watch Out"
+			}
+
+	}
+
+	return Animal;
+})();
+
+var Bird = (function() {
+	function Bird(){
+		Bird.prototype.move = function() {
+			Animal.call(this,name);
+			return "Fly";
+		}
+	}
+
+	Bird.prototype = new Animal;
+	Animal.prototype.constructor = Bird;
+
+	return Bird;
+
+
+})();
+
+var Fish = (function(){
+	function Fish(){
+		Fish.prototype.move = function() {
+			Animal.call(this,name);
+			return "Swim";
+		}
+	}
+
+		Fish.prototype = new Animal;
+		Animal.prototype.constructor = Fish;
+
+		return Fish;
+	
+})();
+
+var Penguin = (function(){
+	function Penguin(){
+		Penguin.prototype.move = function(){
+			Animal.call(this,name);
+			Fish.call(this);
+		}
+	}
+
+	Penguin.prototype.move = new Fish;
+	Animal.prototype = new Animal;
+	Fish.prototype.constructor = Penguin;
+
+	return Penguin;
+
+
+})();
+
+
