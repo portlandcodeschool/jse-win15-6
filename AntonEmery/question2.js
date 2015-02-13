0ba34771a70f9c35292393a9b87a46732a8b3356
AntonEmery <antonemery@gmail.com>
2015-02-12 21:08:11 -0800

    2a who knows

--- AntonEmery/question2.js
+++ AntonEmery/question2.js
@@ -1,38 +1,50 @@
 /* Question 2 */
 
-function Animal() {
+function Animal(name) {
+	this.name = name;
 	};
 	
 	//every subclass needs to inheret move
 
 	Animal.prototype.move = function(){return 'walk';};
 	
+		Bird.prototype = Object.create(Animal.prototype);		
+		//Bird.prototype.constructor = Bird;
 
 	//subclass of animal
 	function Bird() {
+		this.name = name;
 		Animal.call(this);
 		canFly = true;
 		//some sort of if statement? look in Headfirst JS book
 		hasWings = true;
 		this.move = function() {
-			if(hasWings == true){
-				return 'fly';
+		 	if(hasWings == true){
+		 		return 'fly';
 			}
-		}
-	}; 
+		};
+	}
 		
-	
 
-	/*
+				
+			//sub class of bird
+			function Penguin () {
+			Bird.call(this);
+			this.move = function() {
+				return 'swim';
+				}
+			}
+		Penguin.prototype = Object.create(Bird.prototype);		
+		Penguin.prototype = new Bird();
+		Penguin.prototype.constructor = Penguin;
+
 	//subclass of animal
 	function Fish() {
-		move() return swim
-	}
+	Animal.call(this);
+		this.move = function() { 
+			return 'swim';
+			}
+	};
 	
-	
-//sub class of bird
-	function Penguin () {
-		cannot fly
-		move() return swim?
-	}
-*/
+	Fish.prototype = Object.create(Animal.prototype);
+
