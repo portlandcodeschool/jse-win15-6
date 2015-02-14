acb4cfb990071c16b89c3683f82df9d40f3793e5
AntonEmery <antonemery@gmail.com>
2015-02-13 20:50:53 -0800

    2a done

--- AntonEmery/question2.js
+++ AntonEmery/question2.js
@@ -1,49 +1,52 @@
 /* Question 2 */
 
+/* part a */
+
 function Animal(name) {
 	this.name = name;
 	};
 	
 	//every subclass needs to inheret move
-
 	Animal.prototype.move = function(){return 'walk';};
 	
 		
 
-	//subclass of animal
+	//subclass of animal. needs to inherit name
 	function Bird(name) {
-		Animal.call(name);
-		canFly = true;
-		//some sort of if statement? look in Headfirst JS book
-		hasWings = true;
+		Animal.call(this, name);
+		this.name = name;
+		this.canFly = true;
+		this.hasWings = true;
 		this.move = function() {
-		 	if(hasWings == true){
 		 		return 'fly';
 			}
 		};
-	}
-		
-	Bird.prototype = Object.create(Animal.prototype);
+	
+	Bird.prototype = new Animal(); 
+	Bird.prototype.constructor = Bird;
 	
 				
 			//sub class of bird
-			function Penguin () {
-			Bird.call(this);
+			function Penguin(name) {
+			this.name = name;
+			Bird.call(this, name);
+			this.canFly = false;
 			this.move = function() {
 				return 'swim';
 				}
 			}
-		Penguin.prototype = Object.create(Bird.prototype);		
-		Penguin.prototype = new Bird();
+		Penguin.prototype = new Bird();		
 		Penguin.prototype.constructor = Penguin;
 
 	//subclass of animal
-	function Fish() {
+	function Fish(name) {
 	Animal.call(this);
+	this.name = name;
 		this.move = function() { 
 			return 'swim';
 			}
 	};
 	
-	Fish.prototype = Object.create(Animal.prototype);
-
+	Fish.prototype = new Animal();
+	Fish.prototype.constructor - Fish;
+	
